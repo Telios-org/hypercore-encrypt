75ba6957a1958df58cc3b817fb8b4523bc5f16b8
hexadecibal <hex@telios.io>
2021-10-21 20:42:11 -0400

    refactor

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const codecs = require('codecs')
 const c = require('compact-encoding')
 const sodium = require('sodium-native')
 
-module.exports = class HypercoreEncrypt extends Hypercore {
+class HypercoreEncrypt extends Hypercore {
   constructor(storage, key, opts) {
     super(storage, key, opts)
-
-    this.encryption_key = generateAEDKey()
+    this.encryption_key = this._generateAEDKey()
   }
 
   async get(index, opts) {
     if (this.opened === false) await this.opening
     const encoding = (opts && opts.valueEncoding && c.from(codecs(opts.valueEncoding))) || this.valueEncoding
 
-    if (this.core.bitfield.get(index)) return decode(encoding, await this.core.blocks.get(index), this.encryption_key)
+    if (this.core.bitfield.get(index)) return this._decode(encoding, await this.core.blocks.get(index), this.encryption_key)
     if (opts && opts.onwait) opts.onwait(index)
 
-    return decode(encoding, await this.replicator.requestBlock(index), this.encryption_key)
+    return this._decode(encoding, await this.replicator.requestBlock(index), this.encryption_key)
   }
 
   async append(blocks) {
@@ -28,10 +27,10 @@ module.exports = class HypercoreEncrypt extends Hypercore {
     const buffers = new Array(blks.length)
 
     for (let i = 0; i < blks.length; i++) {
-      let buf = encrypt(blks[i], this.encryption_key)
+      let buf = this._encrypt(blks[i], this.encryption_key)
 
       if (!Buffer.isBuffer(blks[i])) {
-        buf = encrypt(c.encode(this.valueEncoding, blks[i]).toString(), this.encryption_key)
+        buf = this._encrypt(c.encode(this.valueEncoding, blks[i]).toString(), this.encryption_key)
       }
 
       buffers[i] = buf
@@ -39,44 +38,46 @@ module.exports = class HypercoreEncrypt extends Hypercore {
 
     return await this.core.append(buffers, this.sign)
   }
+
+  _decode(enc, buf, k) {
+    buf = this._decrypt(buf, k)
+    return enc ? c.decode(enc, buf) : buf
+  }
+
+  _generateAEDKey() {
+    let k = Buffer.alloc(sodium.crypto_aead_xchacha20poly1305_ietf_KEYBYTES)
+    sodium.crypto_aead_xchacha20poly1305_ietf_keygen(k)
+    return k
+  }
+
+  _encrypt(msg, key) {
+    let m = typeof msg === 'object' ? Buffer.from(JSON.stringify(msg)) : Buffer.from(msg, 'utf-8')
+
+    let c = Buffer.alloc(m.length + sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES)
+    let nonce = Buffer.alloc(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES)
+    let k = Buffer.from(key, 'hex')
+
+    sodium.randombytes_buf(nonce)
+
+    sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(c, m, null, null, nonce, k)
+
+    let encrypted = Buffer.from([])
+    encrypted = Buffer.concat([nonce, c], sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES + c.length)
+
+    return encrypted
+  }
+
+  _decrypt(c, k) {
+    // slice nonce out of the encrypted message
+    let nonce = c.slice(0, sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES)
+    let cipher = c.slice(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES, c.length)
+
+    let m = Buffer.alloc(cipher.length - sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES)
+
+    sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(m, null, cipher, null, nonce, k)
+
+    return m
+  }
 }
 
-function decode(enc, buf, k) {
-  buf = decrypt(buf, k)
-  return enc ? c.decode(enc, buf) : buf
-}
-
-function generateAEDKey() {
-  let k = Buffer.alloc(sodium.crypto_aead_xchacha20poly1305_ietf_KEYBYTES)
-  sodium.crypto_aead_xchacha20poly1305_ietf_keygen(k)
-  return k
-}
-
-function encrypt(msg, key) {
-  let m = typeof msg === 'object' ? Buffer.from(JSON.stringify(msg)) : Buffer.from(msg, 'utf-8')
-
-  let c = Buffer.alloc(m.length + sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES)
-  let nonce = Buffer.alloc(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES)
-  let k = Buffer.from(key, 'hex')
-
-  sodium.randombytes_buf(nonce)
-
-  sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(c, m, null, null, nonce, k)
-
-  let encrypted = Buffer.from([])
-  encrypted = Buffer.concat([nonce, c], sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES + c.length)
-
-  return encrypted
-}
-
-function decrypt(c, k) {
-  // slice nonce out of the encrypted message
-  nonce = c.slice(0, sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES)
-  let cipher = c.slice(sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES, c.length)
-
-  let m = Buffer.alloc(cipher.length - sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES)
-
-  sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(m, null, cipher, null, nonce, k)
-
-  return m
-}
+module.exports = HypercoreEncrypt
